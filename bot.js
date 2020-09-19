fb4953d4ba14f90288ea7928b5051aa8541a43af
leafs1 <michael.adragna1@gmail.com>
2020-09-19 19:19:22 -0400

    Added package.json and change auth token

--- bot.js
+++ bot.js
@@ -1,5 +1,5 @@
 const Discord = require('discord.js');
-const auth = require("./auth.json")
+//const auth = require("./auth.json")
 const puppeteer = require('puppeteer');
 
 
@@ -308,4 +308,4 @@ client.on('message', msg => {
   }
 });
 
-client.login(auth.token);
+client.login(process.env.BOT_TOKEN);
